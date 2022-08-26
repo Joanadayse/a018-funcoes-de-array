7b4eb4cef3e0e4884c9e698ccf63d08f18d7d42b
Joanadayse <daysejoana3@gmail.com>
2022-08-26 15:03:08 -0300

    Funcoes018

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const array=[1,2,3,4,5,6,7,8]
+
+const multTres=array.map(item=>item*3)
+console.log(multTres)
+
+const triplos=[3,6,9,12,15,18,21,24]
+
+const divididos=array.map(item=>item%2)
+console.log(divididos)
+
+const metades=[1, 0, 1, 0, 1, 0, 1, 0]
